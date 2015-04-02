8afbfbaf8008c81e20369529aee44c529b73d89b
Brandon Perry <bperry.volatile@gmail.com>
2015-04-01 19:14:00 -0500

    small updates

--- Example/Main.cs
+++ Example/Main.cs
@@ -10,9 +10,10 @@ namespace Example
 	{
 		public static void Main (string[] args)
 		{
-			using (OpenVASSession session = new OpenVASSession ("admin", "passfword", "192.168.1.99")) {
+			using (OpenVASSession session = new OpenVASSession ("admin", "password", "192.168.1.99")) {
 				using (OpenVASManager manager = new OpenVASManager (session)) {
 					Console.WriteLine (manager.GetVersion ().ToString ());
+					Console.WriteLine (manager.GetScanConfigurations ());
 				}
 			}
 		}
--- OpenVASManager.cs
+++ OpenVASManager.cs
@@ -17,7 +17,31 @@ namespace openvassharp
 		public XDocument GetVersion() {
 			return _session.ExecuteCommand (XDocument.Parse ("<get_version />"));
 		}
-		
+
+		public XDocument GetScanConfigurations() {
+			return _session.ExecuteCommand (XDocument.Parse ("<get_configs />"), true);
+		}
+
+		public XDocument CreateSimpleTarget(string cidrRange, string targetName) {
+			return null;
+		}
+
+		public XDocument CreateSimpleTask(string name, string comment, Guid configID, Guid targetID) {
+			return null;
+		}
+
+		public XDocument StartTask(Guid taskID) {
+			return null;
+		}
+
+		public XDocument GetTasks() {
+			return null;
+		}
+
+		public XDocument GetTaskResults(Guid taskID) {
+			return null;
+		}
+
 		public void Dispose()
 		{
 			_session = null;
